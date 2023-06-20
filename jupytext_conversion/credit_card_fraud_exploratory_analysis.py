e66f66b569cf98fab8d3a9bb07b3a6a346eb9f17
Arthur Albarelli <arthuralbarelli@gmail.com>
2023-06-19 22:36:31 -0300

    remove(analysis): Delete data overview section

--- jupytext_conversion/credit_card_fraud_exploratory_analysis.py
+++ jupytext_conversion/credit_card_fraud_exploratory_analysis.py
@@ -66,64 +66,6 @@ print(fraud_data.isnull().sum())
 
 fraud_data.describe().style.format('{:.2f}')
 
-# # Data Overview
-
-# ## Train
-
-fraud_train.head()
-
-fraud_train.info()
-
-# **Highlights:**
-# - There is no null data in the train dataset.
-# - There are 1,296,675 transactions in the train dataset.
-
-# ## Test
-
-fraud_test.head()
-
-fraud_test.info()
-
-# **Highlights:**
-# - There is also no null data in the test dataset.
-# - There are 555,719 transactions in the test dataset.
-# - The test dataset has almost 50% less data than train dataset.
-
-# # Analysis
-
-# ## Target: `is_fraud`
-
-# +
-print('Absolute Values:\n')
-print('Train:')
-print(fraud_train['is_fraud'].value_counts())
-
-print('\n')
-
-print('Test:')
-print(fraud_test['is_fraud'].value_counts())
-
-print('\n=======================================\n')
-
-print('Normalized Values:\n')
-print('Train:')
-print(fraud_train['is_fraud'].value_counts(normalize=True))
-
-print('\n')
-
-print('Test:')
-print(fraud_test['is_fraud'].value_counts(normalize=True))
-# -
-
-# **Highlight:**
-# * Less than 0.5% of all transactions in both datasets (train and test) were fraud.
-
-# ### Charts
-
-sns.countplot(data=fraud_train, x='is_fraud')
-
-sns.countplot(data=fraud_test, x='is_fraud')
-
 # # Univariate analysis
 
 # ## Bar plot for gender
